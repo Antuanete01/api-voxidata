29c5d46328536b12947a1b3c6a7168fb67395cef
Antuanete01 <u202310358@upc.edu.pe>
2026-03-17 15:16:39 -0500

    fix env.ts

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,6 +1,6 @@
 // src/lib/env.ts
 export function env(name: string): string {
-  const v = import.meta.env[name];
+  const v = process.env[name];
   if (!v) throw new Error(`Falta variable de entorno: ${name}`);
   return v;
 }
