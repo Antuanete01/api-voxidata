84bbeb1ba29ea6b89360548ca4f6f879b343bc27
Antuanete01 <u202310358@upc.edu.pe>
2026-03-17 14:47:37 -0500

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
-import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-  output: "server",
+  output: 'server',
   adapter: vercel(),
-  security: {
-    checkOrigin: false,
-  },
 });
